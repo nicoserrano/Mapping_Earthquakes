e3da43095d8e2ddfc03c2ff6bee8ae8a1188e833
Juan Nicolas Serrano <jns132@miami.edu>
2021-08-04 21:05:15 -0400

    final version of logic5

--- Earthquakes_past7days/static/js/logicStep5.js
+++ Earthquakes_past7days/static/js/logicStep5.js
@@ -124,7 +124,7 @@ legend.onAdd = function () {
     for (let i = 0; i < magnitudes.length; i++) {
         console.log(colors[i]);
         div.innerHTML +=
-            "<i style'background: " + colors[i] + "'></i> " + magnitudes[i] + (magnitudes[i+1] ? "&ndash;" + magnitudes[i+1] + "<br>" : "+");
+            "<i style='background: " + colors[i] + "'></i> " + magnitudes[i] + (magnitudes[i+1] ? "&ndash;" + magnitudes[i+1] + "<br>" : "+");
     }
     return div;
 };
